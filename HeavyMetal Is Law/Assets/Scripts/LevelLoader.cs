e5eb2eea081f4c44c648b8ee5ae1cd8ba582d8ac
Keenan <keenan.carroll.va@gmail.com>
2019-03-28 15:25:33 -0400

    Level Loader Script

---
+++ HeavyMetal Is Law/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadMainMenu());
+    }
+
+    IEnumerator LoadMainMenu()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Start Screen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

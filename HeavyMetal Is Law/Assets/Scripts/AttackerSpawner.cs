a4785dc67f2ba06af5e0897f655f0a3febdce4f5
Keenan <keenan.carroll.va@gmail.com>
2019-03-29 10:44:45 -0400

    Enemies Spawn

--- HeavyMetal Is Law/Assets/Scripts/AttackerSpawner.cs
+++ HeavyMetal Is Law/Assets/Scripts/AttackerSpawner.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class AttackerSpawner : MonoBehaviour
 {
+    [SerializeField] GameObject enemy;
+    [SerializeField] int minEnemies;
+    [SerializeField] int maxEnemies;
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 5f;
+
     bool spawn = true;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            SpawnAttacker();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
     }
 }

7fd40c9ec6f7e36086e2a5d7786bcf5f245685c9
Keenan <keenan.carroll.va@gmail.com>
2019-03-29 10:20:18 -0400

    Enemies walk. Setting up spawning

---
+++ HeavyMetal Is Law/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    bool spawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

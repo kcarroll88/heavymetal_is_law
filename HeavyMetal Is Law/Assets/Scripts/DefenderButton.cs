4adf2f7f56088c93ae3bb3bdfd1bfdad26077d1b
Keenan <keenan.carroll.va@gmail.com>
2019-03-30 11:18:12 -0400

    Defender Selector

---
+++ HeavyMetal Is Law/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    [SerializeField] Defender defenderPrefab;
+
+    private void OnMouseDown()
+    {
+        FindObjectOfType<DefenderSpawner>().SetSelectedDefender(defenderPrefab);
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(77, 77, 77, 255);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
+

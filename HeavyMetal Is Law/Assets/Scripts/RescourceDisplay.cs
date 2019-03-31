5480e44a46b718b2e2efe56d58be85cf824b7455
Keenan <keenan.carroll.va@gmail.com>
2019-03-31 10:33:35 -0400

    Resources

---
+++ HeavyMetal Is Law/Assets/Scripts/RescourceDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RescourceDisplay : MonoBehaviour
+{
+    [SerializeField] int resources = 15;
+    Text resourceText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resourceText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        resourceText.text = resources.ToString();
+    }
+
+    public void AddResource(int amount)
+    {
+        resources += amount;
+        UpdateDisplay();
+    }
+
+    public void SubtractResource(int amount)
+    {
+        if (resources >= amount)
+        {
+            resources -= amount;
+            UpdateDisplay();
+        }
+    }
+}

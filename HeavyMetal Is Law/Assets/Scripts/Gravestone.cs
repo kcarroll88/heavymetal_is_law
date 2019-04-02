f747974c1a7e247caaee69ab34cb1af439e5eab7
Keenan <keenan.carroll.va@gmail.com>
2019-04-02 16:03:29 -0400

    fox animations

---
+++ HeavyMetal Is Law/Assets/Scripts/Gravestone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D otherCollider)
+    {
+        Attacker attacker = GetComponent<Attacker>();
+
+        if (attacker)
+        {
+            // TODO do some sort of animation
+        }
+    }
+}

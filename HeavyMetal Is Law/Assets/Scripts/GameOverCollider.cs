7988150256ac2e58a67caecf5809eec2b136915d
Keenan <keenan.carroll.va@gmail.com>
2019-04-03 09:55:43 -0400

    Setting up Game Over

---
+++ HeavyMetal Is Law/Assets/Scripts/GameOverCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverCollider : MonoBehaviour
+{
+    private void OnCollisionEnter2D()
+    {
+        FindObjectOfType<GameOverDisplay>().SubtractLife();
+    }
+}
---
+++ HeavyMetal Is Law/Assets/Scripts/GameOverDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverDisplay : MonoBehaviour
+{
+    [SerializeField] int lives = 5;
+    [SerializeField] int takeLife = 1;
+    Text healthText;
+
+    private void Start()
+    {
+        healthText = GetComponent<Text>();
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        healthText.text = lives.ToString();
+    }
+
+    public void SubtractLife()
+    {
+        lives -= takeLife;
+        UpdateText();
+    }
+}
